de562068a4e0cf7f5ce3d54d19cefd5cc1b4612a
RohanSoni-02 <soni.rohanchd@gmail.com>
2023-02-01 18:09:19 +1100

    Added all lights

--- src/script.js
+++ src/script.js
@@ -29,9 +29,18 @@ scene.add(directionalLight)
 const hemisphereLights = new THREE.HemisphereLight(0xff0000,0x0000ff,0.3)
 scene.add(hemisphereLights)
 
-const pointLight = new THREE.PointLight(0xff9000,0.5)
+const pointLight = new THREE.PointLight(0xff9000,0.5,10,2)
 pointLight.position.set(1,-0.5,1)
 scene.add(pointLight)
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff,2,3,1)
+rectAreaLight.position.set(0,1,3)
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight)
+
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0,2,3)
+scene.add(spotLight)
 
 /**
  * Objects
